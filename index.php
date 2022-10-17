38864d456186e1806bd7b3dfeab2b3b462792b6b
WillRos06 <82073842+WillRos06@users.noreply.github.com>
2022-10-17 18:37:17 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+    $db_connection = pg_connect("host=databaseWilliam port=5432 dbname=postgres user=williamdel password=2181336");
+    $SQLQuery = 'SELECT * FROM Comidas';
+    $RecordSet = pg_query($db_connection, $SQLQuery);
+
+?>
+<h1> Comidas </h1> 
+<table class="table table-bordered">
+    <thead>
+        <th>Nombre</th>
+        <th>Pais</th>
+    </thead>
+    <tbody>
+<?php
+   while ($row = pg_fetch_row($RecordSet)) {
+?>
+         <tr>
+             <td><?php echo $row[1]; ?></td>
+             <td><?php echo $row[2]; ?></td>
+        </tr>
+<?php
+    }
+    pg_close($db_connection);
+?>
+</tbody>
+</table>
