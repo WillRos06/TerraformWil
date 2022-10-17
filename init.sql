15604be43e1078a9e6e3f0e9ef54aca237aabaa5
WillRos06 <82073842+WillRos06@users.noreply.github.com>
2022-10-16 20:57:28 -0400

    Add files via upload

---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Comidas(
+numcontrol BIGINT,
+nombre VARCHAR(45),
+pais VARCHAR(45)
+);
+
+
+INSERT INTO Comidas (numcontrol,nombre,pais)
+ VALUES(1000,'Mangu','RD'),(1001,'Sancocho','RD'),(1002,'Hamburguesas','USA');
